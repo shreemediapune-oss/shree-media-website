2820f064eb05d8449014cc621678de2da6101611
Pradip Dhale <shreemediapune@gmail.com>
2026-01-14 11:47:06 +0530

    Whatsapp Button Push

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import WhatsAppButton from './WhatsAppButton';
 import { Outlet } from 'react-router-dom';
 
 const Layout: React.FC = () => {
@@ -10,6 +11,7 @@ const Layout: React.FC = () => {
             <main style={{ flex: 1 }}>
                 <Outlet />
             </main>
+            <WhatsAppButton />
             <Footer />
         </div>
     );
